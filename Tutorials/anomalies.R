7beb9cdb362c56846836edee70d05b885e667a88
VMO <victormorales@MacBook-Pro-de-Victor.local>
2020-10-14 23:08:07 -0500

    anom

--- Tutorials/anomalies.R
+++ Tutorials/anomalies.R
@@ -37,18 +37,17 @@ GeoScatterplot(data=z,coordx=loc,maxdist=50,numbins=4)
 
 
 #### semivariogram 
-#pdf("evario.pdf")
+
 evariog=GeoVariogram(data=z, coordx=loc,maxdist=maxdist/4)
 plot(evariog$centers,evariog$variograms,ylim=c(0,1),pch=20,xlab="Km",ylab="Semi-variogram")
-#dev.off()
+
+
 
 
 
 CorrParam("GenWend_Matern") 
 NuisParam("Gaussian")
 NuisParam("SkewGaussian")
-
-dd=120
 
 
 corrmodel="GenWend_Matern"
@@ -58,7 +57,7 @@ start=list(mean=mean(z),sill=var(z),nugget=0.10,scale=200)
 fixed=list(smooth=0,power2=1/3.5)
 pcl1=GeoFit(coordx=loc,corrmodel=corrmodel,data=z,
   model="Gaussian",
-  maxdist=dd,optimizer="BFGS",
+ maxpoints=10,optimizer="BFGS",
       start=start,fixed=fixed) 
 pcl1
 ###########
@@ -68,22 +67,32 @@ fixed=list(smooth=0,power2=1/3.5)
 
 pcl2=GeoFit(coordx=loc,corrmodel=corrmodel,data=z,
   likelihood="Marginal",type="Pairwise",model=model,
-  maxdist=dd,optimizer="BFGS",start=start,fixed=fixed)
+  maxpoints=10,optimizer="BFGS",start=start,fixed=fixed)
 pcl2
+
 
 
 
 #######################################################
 ############# residuals ###############################
 res1=GeoResiduals(pcl1)
+
 GeoQQ(res1)
+
+
 evariog=GeoVariogram(data=res1$data,coordx=loc,maxdist=maxdist/4)
+
 GeoCovariogram(res1,show.vario=TRUE, vario=evariog,pch=20)
+
 #######################################################
 res2=GeoResiduals(pcl2)
+
 GeoQQ(res2)
+
 evariog=GeoVariogram(data=res2$data,coordx=loc,maxdist=maxdist/4)
+
 GeoCovariogram(res2,show.vario=TRUE,vario=evariog,pch=20)
+
 #######################################################
 
 
@@ -102,11 +111,12 @@ matrix2$nozero
 
 
 ###### cross  validation
-KK=50
+KK=100
 seed=9
-a1=GeoCV(pcl1, K=KK, n.fold=0.25,seed=seed,sparse=TRUE)
-a2=GeoCV(pcl2, K=KK, n.fold=0.25,seed=seed,sparse=TRUE)
-
+a1=GeoCV(pcl1, K=KK, n.fold=0.25,seed=seed,local=TRUE,maxpoints=100)
+a2=GeoCV(pcl2, K=KK, n.fold=0.25,seed=seed,local=TRUE,maxpoints=100)
+mean(a1$rmse);mean(a2$rmse);
+mean(a1$mae);mean(a2$mae);
 
 ############# prediction map ####################
 Sr1 = Polygon(loc)
@@ -120,25 +130,24 @@ lon_seq=seq(long1,long2,23)
 coords_tot=as.matrix(expand.grid(lon_seq,lat_seq))
 gr.in <- locations.inside(coords_tot, SpP)
 
-pr1<-GeoKrig(loc=gr.in,coordx=loc,corrmodel=corrmodel,mse=TRUE,
-  model="Gaussian",sparse=TRUE,
+pr1<-GeoKrigloc(loc=gr.in,coordx=loc,corrmodel=corrmodel,mse=TRUE,
+  model="Gaussian",sparse=TRUE,maxpoints=100,
   param=as.list(c(pcl1$param,pcl1$fixed)),data=z)
 
-pr2<-GeoKrig(loc=gr.in,coordx=loc,corrmodel=corrmodel,mse=TRUE,
-  model="SkewGaussian",sparse=TRUE,
+pr2<-GeoKrigloc(loc=gr.in,coordx=loc,corrmodel=corrmodel,mse=TRUE,
+  model="SkewGaussian",sparse=TRUE,maxpoints=100,
   param=as.list(c(pcl2$param,pcl2$fixed)),data=z)
 
 par(mfrow=c(2,2))
 
-
 quilt.plot(gr.in,pr1$pred)
-
 
 quilt.plot(gr.in,pr1$mse) 
 
 quilt.plot(gr.in,pr2$pred)
 
 quilt.plot(gr.in,pr2$mse)
+
 
 #######################################
 
