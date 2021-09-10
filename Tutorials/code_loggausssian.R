9b87c5d39be397557d4dde130c703ffe85b8a6e6
VMO <victor.morales@uv.cl>
2021-09-09 19:39:59 -0500

    loggaus

--- Tutorials/code_loggausssian.R
+++ Tutorials/code_loggausssian.R
@@ -1,6 +1,6 @@
 #########################################################
 #########################################################
-# code fot the tutorial on loggaussian  random fields analysis
+# code for the tutorial on loggaussian  random fields analysis
 #########################################################
 #########################################################
 rm(list=ls())
@@ -14,8 +14,9 @@ model="LogGaussian" # model name in the GeoModels package
 
 
 
-N=1000 # number of location sites 
-set.seed(24)
+
+N=2000 # number of location sites 
+set.seed(72)
 x = runif(N, 0, 1)
 y = runif(N, 0, 1)
 coords=cbind(x,y) 
@@ -26,44 +27,46 @@ X=cbind(rep(1,N),runif(N)) # matrix covariates
 
 NuisParam(model,num_betas=2)
 
-mean = -0.3; mean1 =0.5 # regression parameters
+mean = -0.2; mean1 =0.25 # regression parameters
 nugget=0 # nugget parameter
-sill=0.5
+sill=2
 
 
 corrmodel = "Wend0" ## correlation model and parameters 
 CorrParam("Wend0")
-scale = 0.2
+scale = 0.25
 power2 =4
 
 
 
 param=list(mean=mean,mean1=mean1,sill=sill, nugget=nugget, scale=scale ,power2=power2)
-set.seed(312)
 data = GeoSim(coordx=coords , corrmodel=corrmodel , model=model , param=param ,
-X=X,sparse=TRUE)$data
+X=X)$data
 
 start=list(mean=mean,mean1=mean1,scale=scale,sill=sill)
 fixed=list(nugget=nugget ,power2=power2)
+
 # Maximum composite-likelihood fitting of the loggaussian random field:
-fit = GeoFit(data=data,coordx=coords, corrmodel=corrmodel,model=model,X=X,
-optimizer="BFGS",start=start,fixed=fixed,neighb=3)
+fit = GeoFit2(data=data,coordx=coords, corrmodel=corrmodel,model=model,X=X,
+likelihood="Conditional",type="Pairwise",
+start=start,fixed=fixed,neighb=4)
 
 fit
 
-# computing residuals
-res=GeoResiduals(fit) 
+res=GeoResiduals(fit) # computing residuals
+
 #### checking marginal assumptions
+
 GeoQQ(res)
+
 #### checking dependence assumptions: variogram
 vario = GeoVariogram(data=res$data, coordx=coords,maxdist=0.3) # empirical variogram 
-GeoCovariogram(res, show.vario=TRUE, vario=vario,pch=20)
+
+GeoCovariogram(res,show.vario=TRUE, vario=vario,pch=20)
 
 
 
-GeoScatterplot(res$data, coordx=coords,neighb=c(5,15))
-#aa=qnorm(plnorm(res$data))
-#GeoScatterplot(aa, coordx=coords,neighb=c(5,15))
+
 ##################
 ##### prediction
 ##################
@@ -79,13 +82,17 @@ pr=GeoKrig(data=data, coordx=coords,loc=loc_to_pred, X=X,Xloc=Xloc,
 
 
 
+colour = rainbow (100)
+
+
 
 par(mfrow=c(1,3))
-quilt.plot(x, y, data,main="Data") ## map of simulated data 
+quilt.plot(x, y, data,col=colour,main="Data") ## map of simulated data 
 map=matrix(pr$pred,ncol=length(xx))
 map=matrix(pr$pred,ncol=length(xx))
-image.plot(xx, xx, map,xlab="",ylab="",main="Kriging") ## kriging map 
+image.plot(xx, xx, map,col=colour,xlab="",ylab="",main="Kriging") ## kriging map 
 map_mse=matrix(pr$mse,ncol=length(xx)) ## associated MSE kriging map 
-image.plot(xx, xx, map_mse,xlab="",ylab="",main="MSE")
+image.plot(xx, xx, map_mse,col=colour,xlab="",ylab="",main="MSE")
+
 
 
