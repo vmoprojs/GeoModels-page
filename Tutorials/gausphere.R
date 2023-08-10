73cb46256e7db127835e719c2af63f044fadffc2
VMO <victor.morales@uv.cl>
2023-08-09 21:41:41 -0500

    sphere tut

--- Tutorials/gausphere.R
+++ Tutorials/gausphere.R
@@ -1,18 +1,10 @@
 rm(list=ls())
-require(devtools)
-#install_github("vmoprojs/GeoModels")
 library(GeoModels)
 library(mapproj)
 library(globe)
 library(fields)
 library(sphereplot)
 set.seed(1891)
-################################################################
-###
-### Example 8. Simulation of a Gaussian RF 
-###  with a Wend0 correlation on the planet earth
-###
-###############################################################
 
 
 
@@ -46,6 +38,12 @@ data = GeoSim(coordx=coords, corrmodel=corrmodel, param=param,
 
 
 
+param=list(mean=mean, sill=sill, nugget=nugget, smooth=smooth,scale=scale)
+cc = GeoCovmatrix(coordx=coords, corrmodel=corrmodel, param=param,
+             distance="Geod",radius=radius)$covmatrix
+
+
+
 fixed<-list(nugget=nugget)
 start<-list(mean=mean,scale=scale,sill=sill,smooth=smooth)
 
@@ -63,27 +61,23 @@ fit_geo_ml <- GeoFit(data=data,coordx=coords,corrmodel=corrmodel,
               start=start,fixed=fixed,distance="Geod",radius=radius)
 
 
-geod_ds=rdist.earth(coords,miles=F,R=radius)
-max_geod=max(geod_ds)
+
 ## maximum pairwise likelihood estimation using geodesic distance
 fit_geo_pl <- GeoFit(data=data,coordx=coords,corrmodel=corrmodel, 
-             likelihood="Conditional",type="Pairwise", maxdist=max_geod/15,
+             likelihood="Conditional",type="Pairwise", neighb=3,
              optimizer=optimizer,lower=lower,upper=upper,
              start=start,fixed=fixed,distance="Geod",radius=radius)
 
 
-fit_geo_ml$param
+unlist(fit_geo_ml$param)
 
-fit_geo_pl$param
+unlist(fit_geo_pl$param)
 #---------------------------------------------------
 
 
-
-chor_ds=2*sin(geod_ds/2)
-max_chor=max(chor_ds)
 ## maximum pairwise likelihood estimation using chordal  distance
 fit_chor_pl <- GeoFit(data=data,coordx=coords,corrmodel="Matern", 
-                 likelihood="Marginal",type="Pairwise",maxdist=max_chor/15,
+                 likelihood="Marginal",type="Pairwise",neighb=3,
                     optimizer=optimizer,lower=lower,upper=upper,
                     start=start,fixed=fixed,distance="Chor",radius=radius)
 print(fit_chor_pl)
@@ -106,20 +100,22 @@ points(coords_prj,pch=20,cex=0.4)
 
 
 
-eucl_ds=dist(coords_prj)
-max_eucl=max(eucl_ds)
+
 ## maximum pairwise likelihood estimation using euclidean  distance
 distance="Eucl"
 fit_eucl_pl <- GeoFit(data=data,coordx=coords_prj,corrmodel="Matern", 
-                    maxdist=max_eucl/15,likelihood="Marginal",type="Pairwise",
+                    neighb=3,likelihood="Marginal",type="Pairwise",
                     optimizer=optimizer,lower=lower,upper=upper,
                     start=start,fixed=fixed,distance=distance,radius=radius)
 
 
-fit_chor_pl$param
-fit_eucl_pl$param
+unlist(fit_chor_pl$param)
+unlist(fit_eucl_pl$param)
 
 
+geod_ds=rdist.earth(coords,miles=F,R=radius)
+chor_ds=2*sin(geod_ds/2)
+eucl_ds=dist(coords_prj)
 boxplot(c(geod_ds),c(chor_ds),c(eucl_ds))
 
 
@@ -132,20 +128,18 @@ res=GeoResiduals(fit_geo_pl)
 ### checking model assumptions: marginal distribution
 GeoQQ(res)
 ### checking model assumptions: ST variogram model
-vario = GeoVariogram(data=res$data,coordx=coords,maxdist=max_geod/2,distance="Geod",radius=radius)
-GeoCovariogram(res,vario=vario,show.vario=TRUE,pch=20)
+vario = GeoVariogram(data=res$data,coordx=coords,maxdist=max(geod_ds)/2,distance="Geod",radius=radius)
+GeoCovariogram(res,vario=vario,show.vario=TRUE,pch=20,ylim=c(0,1.2))
 
 
 
 
 
-########### predictiob using geodesic distance
-
-loc_to_pred=as.matrix(expand.grid(seq(-180,180,2),seq(60,90,1)))
+loc_to_pred=as.matrix(expand.grid(seq(-180,180,2.5),seq(45,90,0.15)))
 dim(loc_to_pred)
 param_est<-as.list(c(fixed,fit_geo_pl$param))
-pr_geo=GeoKrig(data=data,loc=loc_to_pred,coordx=coords,corrmodel=corrmodel,
-            radius=radius,distance="Geod",param=param_est,mse=TRUE)
+pr_geo=GeoKrigloc(data=data,loc=loc_to_pred,coordx=coords,corrmodel=corrmodel,
+                  radius=radius,distance="Geod",param=param_est,mse=TRUE,neighb=100)
 
 
 predictions=cbind(loc_to_pred,pr_geo$pred,pr_geo$mse)
@@ -153,19 +147,13 @@ head(predictions)
 
 
 
-globeearth(eye=place("newyorkcity"))
-globepoints(loc=loc_to_pred,pch=20,col =  heat.colors(length(pr_geo$pred),alpha=0.1)[rank(pr_geo$pred)])
+globeearth(eye=place("newyorkcity"));
+globepoints(loc=loc_to_pred,pch=20,
+            col = terrain.colors(length(pr_geo$pred),alpha=0.03)[rank(pr_geo$pred)])
 
-
-
-globeearth(eye=place("newyorkcity"))
-globepoints(loc=loc_to_pred,pch=20,col =  cm.colors(length(pr_geo$mse),alpha=0.1)[rank(pr_geo$mse)])
-
-
-
-)
-
-
+globeearth(eye=place("newyorkcity"));
+ globepoints(loc=loc_to_pred,pch=20,
+col = cm.colors(length(pr_geo$mse),alpha=0.03)[rank(pr_geo$mse)])
 
 
 
