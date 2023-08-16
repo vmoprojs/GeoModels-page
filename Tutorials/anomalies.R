a913afafa8ee354c2b2a9f8a1077c9e7deec92e5
VMO <victor.morales@uv.cl>
2023-08-15 19:43:09 -0500

    anomalies

--- Tutorials/anomalies.R
+++ Tutorials/anomalies.R
@@ -1,6 +1,4 @@
 rm(list=ls())
-require(devtools)
-#install_github("vmoprojs/GeoModels")
 require(GeoModels)
 require(fields)
 require(maps)
@@ -60,46 +58,53 @@ upper=list(mean=I,sill=I,nugget=1,scale=I)
 fixed=list(smooth=0.5)
 pcl1=GeoFit2(coordx=loc,corrmodel=corrmodel,data=z,
 likelihood="Conditional",type="Pairwise",model="Gaussian",
- optimizer="nlminb",lower=lower,upper=upper,
-neighb=5, start=start,fixed=fixed)
+ optimizer="nlminb",lower=lower,upper=upper,sensitivity=TRUE,
+neighb=3, start=start,fixed=fixed)
 pcl1
 ###########
+
+
 
 
 start=list(nugget=0,scale=100,mean=mean(z),sill=var(z),skew=0.4)
 fixed=list(smooth=.5)
 
-lower=list(mean=-I,sill=0,skew=-I,scale=0,nugget=0)
-upper=list(mean=I,sill=I,skew=I,scale=I,nugget=1)
+lower2=list(mean=-I,sill=0,skew=-I,scale=0,nugget=0)
+upper2=list(mean=I,sill=I,skew=I,scale=I,nugget=1)
 pcl2=GeoFit2(coordx=loc,corrmodel=corrmodel,data=z,
-  likelihood="Conditional",type="Pairwise",model="SkewGaussian",
-optimizer="nlminb",lower=lower,upper=upper,
-  neighb=5,start=start,fixed=fixed)
+  likelihood="Conditional",type="Pairwise",model="SkewGaussian",sensitivity=TRUE,
+optimizer="nlminb",lower=lower2,upper=upper2,
+  neighb=3,start=start,fixed=fixed)
 pcl2
 
+pcl1_est=GeoVarestbootstrap(pcl1,K=100,
+                             optimizer="nlminb",lower=lower, upper=upper, 
+                             method="Vecchia",M=30,seed=5)
 
 
 
+pcl2_est=GeoVarestbootstrap(pcl2,K=100,
+                             optimizer="nlminb",lower=lower2, upper=upper2, 
+                             method="Vecchia",M=30,seed=5)
+
+pcl1_est$stderr;pcl2_est$stderr;
+pcl1_est$conf.int;pcl2_est$conf.int
+pcl1_est$claic;pcl2_est$claic
 #######################################################
 ############# residuals ###############################
 res1=GeoResiduals(pcl1)
-GeoQQ(res1)
-evariog=GeoVariogram(data=res1$data,coordx=loc,maxdist=maxdist/4)
-GeoCovariogram(res1,show.vario=TRUE, vario=evariog,pch=20)
-
-#######################################################
 res2=GeoResiduals(pcl2)
-GeoQQ(res2)
+
+
+GeoQQ(res1);GeoQQ(res2)
+
+
+evariog=GeoVariogram(data=res1$data,coordx=loc,maxdist=maxdist/4)
+GeoCovariogram(res1,show.vario=TRUE, vario=evariog,pch=20,ylim=c(0,1.3))
 evariog=GeoVariogram(data=res2$data,coordx=loc,maxdist=maxdist/4)
-GeoCovariogram(res2,show.vario=TRUE,vario=evariog,pch=20)
+GeoCovariogram(res2,show.vario=TRUE, vario=evariog,pch=20,ylim=c(0,1.7))
 
 #######################################################
-
-
-
-
-
-
 
 
 ###### cross  validation
