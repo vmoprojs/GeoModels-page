9bc085a73cc84e99fcf21eba8c58429cbe952dd1
VMO <victor.morales@uv.cl>
2023-08-18 19:32:01 -0500

    log gauss

--- Tutorials/code_loggausssian.R
+++ Tutorials/code_loggausssian.R
@@ -6,28 +6,32 @@
 rm(list=ls())
 require(GeoModels)
 require(fields)
-require(hypergeo)
 model="LogGaussian" # model name in the GeoModels package
 
 
 
 
 
-N=2000 # number of location sites 
-set.seed(72)
+N=1500 # number of location sites 
+set.seed(89)
 x = runif(N, 0, 1)
 y = runif(N, 0, 1)
 coords=cbind(x,y) 
 
 plot(coords,pch=20,xlab="",ylab="")
 
+
+
+
 X=cbind(rep(1,N),runif(N)) # matrix covariates 
+mean = -0.2; mean1 =0.25 # regression parameters
+nugget=0 # nugget parameter
+sill=1
+
 
 NuisParam(model,num_betas=2)
 
-mean = -0.2; mean1 =0.25 # regression parameters
-nugget=0 # nugget parameter
-sill=2
+
 
 
 corrmodel = "Wend0" ## correlation model and parameters 
@@ -41,13 +45,15 @@ param=list(mean=mean,mean1=mean1,sill=sill, nugget=nugget, scale=scale ,power2=p
 data = GeoSim(coordx=coords , corrmodel=corrmodel , model=model , param=param ,
 X=X)$data
 
+
+
 start=list(mean=mean,mean1=mean1,scale=scale,sill=sill)
 fixed=list(nugget=nugget ,power2=power2)
 
 # Maximum composite-likelihood fitting of the loggaussian random field:
 fit = GeoFit2(data=data,coordx=coords, corrmodel=corrmodel,model=model,X=X,
 likelihood="Conditional",type="Pairwise",
-start=start,fixed=fixed,neighb=4)
+start=start,fixed=fixed,neighb=1)
 
 fit
 
@@ -55,12 +61,11 @@ res=GeoResiduals(fit) # computing residuals
 
 #### checking marginal assumptions
 
-GeoQQ(res); 
+GeoQQ(res,pch=20) 
 
 #### checking dependence assumptions: variogram
 vario = GeoVariogram(data=res$data, coordx=coords,maxdist=0.3) # empirical variogram 
-
-GeoCovariogram(res,show.vario=TRUE, vario=vario,pch=20)
+GeoCovariogram(res,show.vario=TRUE, vario=vario,pch=20,ylim=c(0,2.5))
 
 
 
@@ -80,7 +85,7 @@ pr=GeoKrig(data=data, coordx=coords,loc=loc_to_pred, X=X,Xloc=Xloc,
 
 
 
-colour = rainbow (100)
+colour = terrain.colors (20)
 
 
 
